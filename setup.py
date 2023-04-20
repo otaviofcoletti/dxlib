a20a950ef587ec9111d411ae092058cfbc61e440
Rafael Zimmer <rzimmerdev@gmail.com>
2023-04-20 11:55:49 -0300

    Added script for CI

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 # This call to self.setup() does all the work
 setup(
     name="dxlib",
-    version="0.1.0",
+    version="0.1.1",
     description="Quantitative Methods for Finance",
     long_description=long_description,
     long_description_content_type="text/markdown",
