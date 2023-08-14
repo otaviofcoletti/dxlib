8b5eef4652418741d94173218e6906d15803bf99
Rafael Zimmer <rzimmerdev@gmail.com>
2023-08-14 20:43:30 -0300

    Fixed manager http server error handling

--- dxlib/api/server.py
+++ dxlib/api/server.py
@@ -1,14 +1,42 @@
 import http.server
 import inspect
 import json
+import queue
 import socket
 import socketserver
 import threading
 from inspect import signature
 from functools import wraps
 from urllib.parse import parse_qs, urlparse
+from enum import Enum
 
 from ..core.logger import no_logger
+
+
+class ServerStatus(Enum):
+    ERROR = -1
+    STARTED = 0
+    STOPPED = 1
+
+
+def handle_exceptions_decorator(func):
+    def wrapper(self, *args, **kwargs):
+        try:
+            return func(self, *args, **kwargs)
+        except Exception as e:
+            self.handle_exception(e)
+    return wrapper
+
+
+class ExceptionContext:
+    def __init__(self, server):
+        self.server = server
+
+    def __enter__(self):
+        return self.server.get_exceptions()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
 
 
 def endpoint(route_name, description=None):
@@ -40,6 +68,9 @@ class Server:
             self.set_endpoints()
 
         self.port = port if port else self._get_free_port()
+
+        self.exception_queue = queue.Queue()
+        self.exceptions = ExceptionContext(self)
 
         self._started = threading.Event()
         self._error = threading.Event()
@@ -75,6 +106,8 @@ class Server:
             endpoints = self.endpoints
             manager = self.manager
 
+            exception_queue = self.exception_queue
+
             def list_endpoints(self):
                 endpoint_data = {
                     "message": "Available endpoints:",
@@ -87,6 +120,16 @@ class Server:
                 }
                 return json.dumps(endpoint_data, indent=4)
 
+            def handle_exception(self, e):
+                self.exception_queue.put(e)
+                self.send_response(500)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+                self.wfile.write(json.dumps({
+                    "error": str(e)
+                }).encode())
+
+            @handle_exceptions_decorator
             def do_GET(self):
                 if self.path == "/":
                     response = self.list_endpoints()
@@ -108,11 +151,11 @@ class Server:
                     }).encode())
 
                 method_info = self.endpoints[route_name]
-                method = getattr(self.manager, method_info.get('method_name'))
+                method_signature = method_info.get("params", [])
+                method_callable = getattr(self.manager, method_info.get('method_name'))
                 params = parse_qs(path_parts.query)
-                signature = method_info.get("params", [])
 
-                required_params = [param for param, details in signature.items()
+                required_params = [param for param, details in method_signature.items()
                                    if details.default == inspect.Parameter.empty and param != 'self']
 
                 missing_params = set(required_params) - set(params.keys())
@@ -128,7 +171,8 @@ class Server:
                     return
 
                 try:
-                    response = method(**{k: v[0] for k, v in params.items() if k in signature.parameters and k != 'self'})
+                    response = method_callable(**{k: v[0] for k, v in params.items()
+                                                  if k in method_signature.parameters and k != 'self'})
                     self.send_response(200)
                     self.send_header('Content-type', 'application/json')
                     self.end_headers()
@@ -141,7 +185,9 @@ class Server:
                     self.wfile.write(json.dumps({
                         "error": str(e)
                     }).encode())
+                    raise e
 
+            @handle_exceptions_decorator
             def do_POST(self):
                 content_length = int(self.headers['Content-Length'])
                 post_data = self.rfile.read(content_length).decode()
@@ -166,30 +212,41 @@ class Server:
 
         try:
             with socketserver.TCPServer(("", self.port), SimulationManagerHTTPRequestHandler) as self._httpd_server:
-                self._started.set()
                 self.logger.info(f"Server started on port {self.port}")
                 self._httpd_server.serve_forever()
         except Exception as e:
-            print(f"Error starting server: {e}")
+            self.logger.error(f"Server error: {e}")
             self._error.set()
+            self.exception_queue.put(e)
 
-    def start(self):
+    def get_exceptions(self):
+        try:
+            return self.exception_queue.get_nowait()
+        except queue.Empty:
+            return None
+
+    def start(self) -> ServerStatus:
+        self.logger.info(f"Server starting on port {self.port}")
         self._httpd_thread = threading.Thread(target=self._serve)
         self._httpd_thread.start()
+        self._started.set()
+        return ServerStatus.STARTED
 
-    def stop(self):
+    def stop(self) -> ServerStatus:
         if self._error.is_set():
-            self.logger.warning("Could not stop server. Server might not have started properly.")
-            return
+            self.logger.warning("Could not stop server. Server might not have started properly")
+            return ServerStatus.ERROR
 
         self._started.wait()
 
         if self._httpd_server is not None:
+            self.logger.info("Stopping server")
             self._httpd_server.shutdown()
             self._httpd_server.server_close()
             self._httpd_server = None
         if self._httpd_thread is not None:
             self._httpd_thread.join()
+        return ServerStatus.STOPPED
 
     def __del__(self):
         self.stop()
