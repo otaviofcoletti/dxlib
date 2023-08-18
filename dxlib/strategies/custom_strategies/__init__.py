59eca5c965d2a54461c379ad0a04ea87f00f5720
Rafael Zimmer <rzimmerdev@gmail.com>
2023-08-18 16:28:15 -0300

    Small fix to portfolio history loading

--- dxlib/strategies/custom_strategies/__init__.py
+++ dxlib/strategies/custom_strategies/__init__.py
@@ -3,3 +3,4 @@ from .pair_trading import PairTradingStrategy
 from .systematic_trading import SystematicRandomForest
 from .trend_follow import TrendFollowStrategy
 from .volatility_breakout import VolatilityBreakoutStrategy
+from .bollinger_breakout import BollingerBreakoutStrategy
