f59571c8cbd5a7b25b867441abf0cbf84468881d
Rafael Zimmer <rzimmerdev@gmail.com>
2023-08-18 17:39:08 -0300

    Removed upload scripts

--- upload_lib.sh
+++
@@ -1,9 +0,0 @@
-rm -rf dist/*.tar.gz
-rm -rf dist/*.whl
-
-python setup.py sdist bdist_wheel
-
-twine check dist/*
-
-twine upload --skip-existing --repository-url https://test.pypi.org/legacy/ dist/*
-# twine upload --skip-existing dist/*
