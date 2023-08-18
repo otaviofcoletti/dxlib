5b4ad7a438d6271b95039fcdf1ed8851b5743d1b
Rafael Zimmer <rzimmerdev@gmail.com>
2023-08-18 17:14:51 -0300

    Testing new packaging system

--- setup.py
+++ setup.py
@@ -25,8 +25,8 @@ if "-" in remote_version:
 assert "-" not in remote_version
 assert "." in remote_version
 
-assert os.path.isfile("cf_remote/version.py")
-with open("remote/VERSION", "w", encoding="utf-8") as fh:
+assert os.path.isfile("dxlib/version.py")
+with open("dxlib/VERSION", "w", encoding="utf-8") as fh:
     fh.write("%s\n" % remote_version)
 
 setup(
