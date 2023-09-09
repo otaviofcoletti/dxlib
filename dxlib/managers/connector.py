254d7a1c992562906fdb247a6379125c536b37da
Rafael Zimmer <rzimmerdev@gmail.com>
2023-09-09 03:20:16 -0300

    Finished connector class

--- dxlib/managers/connector.py
+++ dxlib/managers/connector.py
@@ -1,6 +1,7 @@
 import threading
 import asyncio
 import websockets
+import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
@@ -8,7 +9,7 @@ class Connector:
     def __init__(self):
         self.servers: dict[str, str] = {}
         self.clients: dict[str, websockets.WebSocketClientProtocol] = {}
-        self.stop_event = threading.Event()
+        self.running = threading.Event()
         self.http_server = None
 
     def add_server(self, name, uri):
@@ -18,9 +19,10 @@ class Connector:
         uri = self.servers.get(name)
         if uri:
             async with websockets.connect(uri) as websocket:
-                while not self.stop_event.is_set():
+                while self.running.is_set():
                     try:
                         message = await websocket.recv()
+                        print("Snapshot received: ", message)
                         await self.forward_to_clients(message)
                     except Exception as e:
                         print(f"Error receiving from {name}: {e}")
@@ -35,14 +37,15 @@ class Connector:
         async def connect_client():
             async with websockets.connect(uri) as websocket:
                 self.clients[uri] = websocket
-                while not self.stop_event.is_set():
+                while self.running.is_set():
                     try:
                         pass
                     except Exception as e:
                         print(f"Error receiving from {uri}: {e}")
                         break
 
-        asyncio.run(connect_client())
+        # non-blocking client websocket creation
+        threading.Thread(target=asyncio.run, args=(connect_client(),)).start()
 
     def remove_client(self, websocket):
         self.clients.pop(websocket)
@@ -57,30 +60,34 @@ class Connector:
                     self.send_response(200)
                     self.end_headers()
                     self.wfile.write(bytes(str(self.connector.servers), "utf-8"))
-                    return
                 # path is of format '/{server_name}'
                 elif self.path.startswith("/?server="):
                     server = self.path.split("=")[1]
                     self.send_response(200)
                     self.end_headers()
-                    self.wfile.write(bytes(server, "utf-8"))
-                    return
+                    self.wfile.write(bytes(self.connector.servers.get(server), "utf-8"))
                 # path is not accepted
-                return self.send_response(404)
+                else:
+                    self.send_response(404)
 
             def do_POST(self):
                 # Test if posting a client, specifically with data = {'uri': 'wss://...'}
                 if self.path == "/":
-                    content_length = int(self.headers["Content-Length"])
-                    post_data = self.rfile.read(content_length)
-                    self.send_response(200)
-                    self.end_headers()
-                    self.connector.add_client(post_data.decode("utf-8"))
-                    return
+                    try:
+                        content_length = int(self.headers["Content-Length"])
+                        post_data = self.rfile.read(content_length)
+                        client_uri = json.loads(post_data)["uri"]
+
+                        self.connector.add_client(client_uri)
+                        self.send_response(200)
+                        self.end_headers()
+                    except RuntimeError as e:
+                        print(e)
+                        self.send_response(400)
+                        self.end_headers()
                 else:
                     self.send_response(404)
                     self.end_headers()
-                    return
 
         def run_http_server():
             server_address = ("localhost", http_port)
@@ -90,8 +97,8 @@ class Connector:
         http_thread = threading.Thread(target=run_http_server)
         http_thread.start()
 
-        # Start WebSocket server connections
         threads = []
+        self.running.set()
         for server_name in self.servers.keys():
             thread = threading.Thread(target=asyncio.run, args=(self.connect_server(server_name),))
             thread.start()
@@ -101,7 +108,7 @@ class Connector:
             thread.join()
 
     def stop(self):
-        self.stop_event.set()
+        self.running.clear()
 
 
 if __name__ == "__main__":
