2ff89a0da1f9220900e1272d33d75adf9e295b61
Rafael Zimmer <rzimmerdev@gmail.com>
2023-10-27 11:47:15 -0300

    Frame for history reconstruction

--- dxlib/core/portfolio/transforms.py
+++ dxlib/core/portfolio/transforms.py
@@ -2,6 +2,17 @@
 
 
 # history reconstruction
+class HistoryReconstruction:
+    def quantities(self):
+        pass
+
+    def value(self):
+        pass
+
+    def trades(self):
+        pass
+
+    # cant reconstruct transactions from trades, value, quantities or orders
 
 # metrics
 
