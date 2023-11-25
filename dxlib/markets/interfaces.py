0759f19e07e2f284e0afd50066a4962ee8b57a5b
Rafael Zimmer <rzimmerdev@gmail.com>
2023-11-25 02:33:22 -0300

    Working order interface

--- dxlib/markets/interfaces.py
+++ dxlib/markets/interfaces.py
@@ -9,14 +9,33 @@ from ..core.trading.order import OrderData, Order
 
 class MarketInterface(ABC):
     def __init__(self):
+        """
+        MarketInterface is an abstract class that defines the interface for a market.
+
+        A market is a source of data for a security and can be used to get historical data or live data.
+        Also provides a way to subscribe to a security, and get data updates.
+        """
         pass
 
     @property
     @abstractmethod
     def history(self) -> History:
+        """
+        Returns the history of the market.
+        Returns:
+            History: A history object that contains the data for the market, as well as methods for manipulating it.
+        """
         pass
 
     def subscribe(self, security):
+        """
+        Subscribes to a security and starts receiving data updates.
+        Args:
+            security: The security to subscribe to.
+
+        Returns:
+            An observable that emits data updates for the security.
+        """
         pass
 
 
