a40f77f56432be93fe6bf2ba1bfd6fe98a9ac2c3
Rafael Zimmer <rzimmerdev@gmail.com>
2024-01-05 13:02:27 -0300

    Refactored Executor

--- dxlib/managers/strategy_manager.py
+++ dxlib/managers/strategy_manager.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import asyncio
 import concurrent.futures
-import threading
 from typing import AsyncGenerator, Generator
 
 import numpy as np
@@ -10,7 +9,8 @@ import pandas as pd
 import websockets
 
 from ..core import Portfolio, History
-from ..core.portfolio.inventory import Inventory
+from ..core.logger import LoggerMixin
+from dxlib.core.components.inventory import Inventory
 from ..servers import WebsocketServer
 from ..servers.endpoint import Endpoint
 from ..strategies import Strategy
@@ -18,20 +18,14 @@ from .manager import Manager, MessageHandler
 
 
 class StrategyManager(Manager):
-    def __init__(self,
-                 strategy,
-                 *args,
-                 **kwargs
-                 ):
-        super().__init__(
-            *args,
-            **kwargs)
+    def __init__(self, strategy, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
         self.strategy: Strategy = strategy
         self.portfolios: dict[str, Portfolio] = {}
         self.executors: set[Executor] = set()
 
-        for comm in kwargs.get('comms', []):
+        for comm in kwargs.get("comms", []):
             if isinstance(comm, WebsocketServer) and comm.handler is None:
                 comm.handler = StrategyMessageHandler(self, comm)
 
@@ -40,7 +34,10 @@ class StrategyManager(Manager):
         if identifier:
             return self.portfolios[identifier]
 
-        return {identifier: portfolio.to_dict() for identifier, portfolio in self.portfolios.items()}
+        return {
+            identifier: portfolio.to_dict()
+            for identifier, portfolio in self.portfolios.items()
+        }
 
     @Endpoint.get("portfolios", "Gets the currently registered portfolios")
     def get_values(self, identifier: str, prices: dict[str, float] | None = None):
@@ -49,7 +46,10 @@ class StrategyManager(Manager):
         elif identifier:
             raise ValueError(f"Portfolio {identifier} not registered")
 
-        return {identifier: portfolio.value(prices) for identifier, portfolio in self.portfolios.items()}
+        return {
+            identifier: portfolio.value(prices)
+            for identifier, portfolio in self.portfolios.items()
+        }
 
     @Endpoint.post("portfolios", "Registers a portfolio with the strategy manager")
     def register_portfolio(self, portfolio: Portfolio | dict, identifier: str = None):
@@ -72,11 +72,13 @@ class StrategyManager(Manager):
 
         return portfolio.accumulate()
 
-    def run(self,
+    def run(
+            self,
             obj: History | Generator | AsyncGenerator | pd.DataFrame | np.ndarray | dict,
             threaded=False,
             executor: Executor = None,
-            position: Inventory = None) -> History | Generator | AsyncGenerator | concurrent.futures.Future | None:
+            position: Inventory = None,
+    ) -> History | Generator | AsyncGenerator | concurrent.futures.Future | None:
         if position is None:
             position = self.position
 
@@ -87,127 +89,82 @@ class StrategyManager(Manager):
         return executor.run(obj, threaded)
 
 
-class Executor:
-    def __init__(self, strategy: Strategy, position: Inventory = None, logger=None):
+class Executor(LoggerMixin):
+    def __init__(self, strategy: Strategy = None, position: Inventory = None, logger=None):
+        super().__init__(logger)
         self.strategy = strategy
-        self.logger = logger if logger else info_logger(__name__)
         self._position = position
-        self._history = None
-
-        self._running = threading.Event()
+        self._history = History()
 
     @property
     def history(self):
         return self._history
 
     @history.setter
-    def history(self, value: History | pd.DataFrame | np.ndarray | dict):
-        self._history = self.format(value)
+    def history(self, value: History):
+        self._history = value
 
-    def run(self,
-            obj: History | Generator | AsyncGenerator | pd.DataFrame | np.ndarray | dict,
-            threaded=False) -> History | Generator | AsyncGenerator | concurrent.futures.Future | None:
-        obj = self.format(obj) if isinstance(obj, (pd.DataFrame, np.ndarray, dict)) else obj
+    def run(
+            self,
+            obj: History | Generator | AsyncGenerator,
+            in_place: bool = False,
+    ) -> pd.Series | Generator | AsyncGenerator | None:
         if obj is None:
-            return
+            raise ValueError("Cannot run strategy on None")
+        if self.strategy is None:
+            raise ValueError("No strategy set")
 
-        if not self._history:
+        if not in_place:
             self._history = History()
 
-        self._running.set()
-        if threaded:
-            with concurrent.futures.ThreadPoolExecutor() as executor:
-                future = executor.submit(self._consume, obj)
-                return future
-        else:
-            return self._consume(obj)
-
-    def stop(self):
-        self._running.clear()
-
-    def _consume(self, obj: History | Generator | AsyncGenerator) -> History | Generator | AsyncGenerator | None:
         if isinstance(obj, History):
-            return self._consume_sync(obj)
+            return self._consume(obj)
         elif isinstance(obj, Generator):
-            return self._consume_subscription(obj)
+            return self._consume_sync(obj)
         elif isinstance(obj, AsyncGenerator):
-            return self._consume_async_subscription(obj)
-        else:
-            raise TypeError(f"Object {obj} is not a valid type")
+            return self._consume_async(obj)
 
-    @staticmethod
-    def format(obj: History | pd.DataFrame | np.ndarray | dict):
-        if isinstance(obj, dict):
-            obj = History.from_dict(obj)
-        elif isinstance(obj, (pd.DataFrame, np.ndarray)):
-            obj = History(obj)
+    def _consume(self, obj: History) -> pd.Series:
+        signals = pd.Series()
 
-        return obj
-
-    def _consume_sync(self, obj: History) -> History | None:
-        signals_history = History()
-        dates = obj.get_level('date')
-
-        try:
-            for date in sorted(dates):
-                if not self._running.is_set():
-                    return signals_history
-                bars = obj.get(dates=[date])
-                self._history += bars
-
-                signals = self.strategy.execute(date, self._position, self._history)
-
-                signals_history += signals
-        except:
-            self.logger.exception("Error in strategy execution")
-        finally:
-            self._running.clear()
-            return signals_history
-
-    def _consume_subscription(self, obj: Generator):
         try:
             for bar in obj:
-                if not self._running.is_set():
-                    break
-                bar_df = self._transform_bar(bar)
-                self._history += bar_df
-                signals = self._process_bar(bar_df)
+                idx = bar[0]
+                bar_df = bar[1]
+                signals[idx] = self._consume_bar(idx, bar_df)
+        finally:
+            return signals
+
+    def _consume_sync(self, obj: Generator):
+        try:
+            for bar in obj:
+                idx = bar[0]
+                bar_df = bar[1]
+                signals = self._consume_bar(idx, bar_df)
                 yield signals
         finally:
-            self._running.clear()
+            return
 
-    async def _consume_async_subscription(self, obj: AsyncGenerator):
+    async def _consume_async(self, obj: AsyncGenerator):
         try:
             async for bar in obj:
-                if not self._running.is_set():
-                    break
-                bar_df = self._transform_bar(bar)
-                self._history += bar_df
-                signals = self._process_bar(bar_df)
+                idx = bar[0]
+                bar_df = bar[1]
+                signals = self._consume_bar(idx, bar_df)
                 yield signals
         finally:
-            self._running.clear()
+            return
 
-    def _transform_bar(self, bar):
-        bar_df = pd.DataFrame.from_dict(bar, orient='index')
-        bar_df.index = pd.MultiIndex.from_tuples(
-            [(bar_df.index.name, list(self._history.security_manager.get(security).values())[0]) for security in
-             bar_df.index])
-
-        return bar_df
-
-    def _process_bar(self, bar_df):
-        date = bar_df.index.get_level_values('date').unique().tolist()[0]
-        date_idx = self._history.get_level('date').sorted().index(date)
-
-        signals = self.strategy.execute(date_idx, self._position, self._history)
-        signals_df = pd.DataFrame(signals, columns=['signal'])
-
-        return signals_df
+    def _consume_bar(self, idx, bar_df):
+        self._history += bar_df
+        signals = self.strategy.execute(idx, self._position, self._history)
+        return signals
 
 
 class StrategyMessageHandler(MessageHandler):
-    def __init__(self, manager: StrategyManager, websocket_server: WebsocketServer = None):
+    def __init__(
+            self, manager: StrategyManager, websocket_server: WebsocketServer = None
+    ):
         super().__init__()
         self.manager = manager
         self.websocket_server = websocket_server
