464e0e0ec6df8a6d0f7b1c5d32b297507f9a2c90
Rafael Zimmer <rzimmerdev@gmail.com>
2024-01-08 18:09:42 -0300

    Test suite for history

--- tests/strategy/test_simple_strategy.py
+++ tests/strategy/test_simple_strategy.py
@@ -10,24 +10,55 @@ class TestSimpleStrategy(unittest.TestCase):
         def __init__(self):
             super().__init__()
 
+        @staticmethod
+        def get_state(history: dx.History, idx):
+            universe = history.level_unique(dx.HistoryLevel.SECURITY)
+
+            levels = {
+                dx.HistoryLevel.DATE: [idx],
+            }
+
+            prices = history.get(levels, fields=["close"]).df
+
+            return universe, prices
+
         def execute(
-            self, idx, position: dx.Inventory, history: dx.History
+            self, bar, position: dx.Inventory, history: dx.History
         ) -> pd.Series:
-            # Simple strategy, buy and hold for every asset in the universe
-            universe = history.get_level(dx.HistoryLevel.SECURITY)
-            return pd.Series(
-                dx.Signal(
-                    side=dx.Side.BUY,
-                    quantity=1,
-                    price=history.get_raw(
-                        securities=universe, fields=["close"], dates=[idx]
-                    ),
-                ),
-                index=universe,
-            )
+            idx, bar = bar
+            universe, prices = self.get_state(history, idx)
+
+            signals = {
+                security: dx.Signal(
+                    dx.Side.BUY, 1, prices.loc[idx, security]
+                ) for security in universe
+            }
+
+            return pd.Series(signals)
 
     def test_execute(self):
-        pass
+        strategy = self.SimpleStrategy()
+        schema = dx.HistorySchema(
+            levels=[dx.HistoryLevel.DATE, dx.HistoryLevel.SECURITY],
+            fields=["close"],
+            security_manager=dx.SecurityManager.from_list(["AAPL", "MSFT"]),
+        )
+
+        inventory = dx.Inventory({
+          security: 0 for security in schema.security_manager
+        })
+
+        history = dx.History({
+            (pd.Timestamp("2021-01-01"), "AAPL"): {"close": 100},
+            (pd.Timestamp("2021-01-01"), "MSFT"): {"close": 200},
+            (pd.Timestamp("2021-01-02"), "AAPL"): {"close": 110},
+            (pd.Timestamp("2021-01-02"), "MSFT"): {"close": 210},
+        }, schema)
+
+        executor = dx.Executor(strategy, inventory, schema)
+        signals = executor.run(history)
+
+        print(signals)
 
 
 if __name__ == "__main__":
